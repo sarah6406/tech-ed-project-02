8d727b57f54919d7687bc52f921a5bdc4b508bf1
Sarah Wiseman <s.wiseman777@gmail.com>
2024-03-28 13:26:04 +0000

    basic plan of cookie clicker game

---
+++ app.js
@@ -0,0 +1,39 @@
+// get elements by ID
+const cookieBtn = 
+const upgradeBtn
+const cookieSpan
+const cpsSpan
+
+//make an object to keep track of cookie count
+
+const stats = {
+
+}
+
+// function to buy cookie
+
+//function to buy upgrade
+
+
+// addEventlistener for buy cookie
+
+// addEventlistener for buy upgrade
+//      remember to subtract amount of cookies from cookie count
+
+// DOM yto disable button until there are enough cookies to buy the upgrade
+
+if (cookieupgrade - cookiecount < 0) {
+    p-tag.textContent = "You do not have enough cookies!"
+}
+
+//function to update page
+
+//function to update local storage to be call within buyCookie and buyUpgrade
+
+//start the timer that will run every second forever
+setInterval(function() {
+    stats.cookieCount += stats.cps;
+    // update page function
+    // update storage function
+
+}, 1000)
